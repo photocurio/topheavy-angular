b67cea4c627918339c503479e9dfd4e09d87d5cd
photocurio <sunraydust@gmail.com>
2017-02-05 14:12:44 -0500

    removed buggy Material layout styles and classes. wrote simple flexbox styles for layout

--- src/routes.js
+++ src/routes.js
@@ -2,24 +2,21 @@ module.exports = routesConfig;
 
 /** @ngInject */
 function routesConfig($locationProvider, $urlRouterProvider, $stateProvider) {
-  $locationProvider
-    .html5Mode(true)
-    .hashPrefix('!');
 
-  $urlRouterProvider
-    .otherwise('/');
+  $locationProvider.html5Mode(true).hashPrefix('!');
 
-  $stateProvider
-    .state('app', {
-      url: '/',
-      component: 'postsInCategory'
-    })
-    .state('category', {
-      url: '/:slug',
-      component: 'postsInCategory'
-    })
-    .state('single', {
-      url: '/:year/:month/:slug',
-      component: 'singlePost'
-    });
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider.state('app', {
+    url: '/',
+    component: 'postsInCategory'
+  })
+  .state('category', {
+    url: '/:slug',
+    component: 'postsInCategory'
+  })
+  .state('single', {
+    url: '/:year/:month/:slug',
+    component: 'singlePost'
+  });
 }
